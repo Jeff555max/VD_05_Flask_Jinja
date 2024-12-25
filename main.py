7b9edd3a0feb647ad44212c3fb117c0f32a81d3d
Jeff555max <vadik98765@gmail.com>
2024-12-25 16:36:36 +0500

    add code

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
 
-@app.route("/new/")
-def new():
-    return "new page"
+@app.route("/")
+def films():
+    return render_template("index.html")
+
+
+@app.route("/person/")
+def person():
+    return render_template("main.html")
+
 
 if __name__ == "__main__":
     app.run()
