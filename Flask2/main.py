36bb1eed3d4e03a2a6af1d8ea37aecd26d71a03b
Jeff555max <vadik98765@gmail.com>
2025-01-10 19:05:16 +0500

    2

--- Flask2/main.py
+++ Flask2/main.py
@@ -4,15 +4,24 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    context = {
+        'question': 'Дубровники',
+    }
+    return render_template("index.html", **context) #распаковываем словарь и пробрасываем наши переменные
 
 @app.route('/blog/')
 def blog():
-    return render_template('blog.html')
+    context = {
+        'question': 'Хорватия и Черногория'
+    }
+    return render_template("blog.html", **context) #распаковываем словарь и пробрасываем наши переменные)
 
 @app.route('/contacts/')
 def contacts():
-    return render_template('contact.html')
+    context = {
+        'question': 'Контакты'
+    }
+    return render_template("contact.html", **context) #распаковываем словарь и пробрасываем наши переменные)
 
 if __name__ == '__main__':
     app.run(debug=True)
